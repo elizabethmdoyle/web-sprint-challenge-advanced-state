cc2521964415ed4296ba212596386b115bcf3a84
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2023-12-19 17:24:53 -0500

    working on project

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,31 +1,40 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
 
-const initialWheelState = 0
-function wheel(state = initialWheelState, action) {
-  return state
-}
-
-const initialQuizState = null
-function quiz(state = initialQuizState, action) {
-  return state
-}
-
-const initialSelectedAnswerState = null
-function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
-}
-
-const initialMessageState = ''
-function infoMessage(state = initialMessageState, action) {
-  return state
-}
-
+const initialWheelState = 0;
+const initialQuizState = null;
+const initialSelectedAnswerState = null;
+const initialMessageState = '';
 const initialFormState = {
   newQuestion: '',
   newTrueAnswer: '',
   newFalseAnswer: '',
 }
+
+
+
+function wheel(state = initialWheelState, action) {
+  return state
+}
+
+
+
+function quiz(state = initialQuizState, action) {
+  return state
+}
+
+
+function selectedAnswer(state = initialSelectedAnswerState, action) {
+  return state
+}
+
+
+function infoMessage(state = initialMessageState, action) {
+  return state
+}
+
+
+
 function form(state = initialFormState, action) {
   return state
 }
